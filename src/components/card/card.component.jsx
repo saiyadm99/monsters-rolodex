43c56b31faaf7d7f5f9f46062a17da76ef1cabaf
saiyadm99 <syedmohammod52@gmail.com>
2022-02-14 18:30:05 +0600

    1.0

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,7 @@ import './card.styles.css'
 
 export const Card = (props) => (
 	<div className="card-container">
-		<img src={`https://robohash.org/${props.monster.id}size=180*180`} alt="" />
+		<img src={`https://robohash.org/${props.monster.id}?set=set2&?size=180*180`} alt="" />
 		<h2>{props.monster.name}</h2>
 		<p>{props.monster.email}</p>
 	</div>
